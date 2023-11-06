c79bc458f36ea929d25f8ac537f911bec353191c
amiokuefe1 <amiokuefe@gmail.com>
2023-11-06 21:30:38 +0100

    Add project files.

---
+++ DevOps_Test/Controllers/TodoController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DevOps_Test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+        public TodoController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Todo>> GetTodos()
+        {
+            var response = await _httpClient.GetAsync("https://jsonplaceholder.typicode.com/todos/");
+            response.EnsureSuccessStatusCode();
+
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            var todos = JsonConvert.DeserializeObject<IEnumerable<Todo>>(jsonResponse);
+
+            return todos;
+        }
+
+        // GET api/<TodoController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<TodoController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<TodoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<TodoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        public class Todo
+        {
+            public int UserId { get; set; }
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public bool Completed { get; set; }
+        }
+    }
+}
